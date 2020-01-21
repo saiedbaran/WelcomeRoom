3fbdae6447a2be9d98dc4209732bb5e3a04f427f
Saeed Safikhani <s.safikhani@outlook.com>
2020-01-21 12:55:34 +0100

    Restructure some folders 21.01.2020

---
+++ Assets/Test_Thumb.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Thumb : MonoBehaviour
+{
+    [SerializeField] GameObject FingerRig;
+    Vector3 initialTransformDelta;
+    Vector3 initialRotationDelta;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialTransformDelta = transform.position - FingerRig.transform.position;
+        initialRotationDelta = transform.rotation.eulerAngles - FingerRig.transform.rotation.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = FingerRig.transform.position + initialTransformDelta;
+        transform.rotation = Quaternion.Euler(FingerRig.transform.rotation.eulerAngles + initialRotationDelta);
+    }
+}
