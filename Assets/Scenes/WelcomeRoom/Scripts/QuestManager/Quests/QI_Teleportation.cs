21f3f927eb210ac2c36bd0b2873aae6590c50187
Saeed Safikhani <s.safikhani@outlook.com>
2020-01-26 17:33:55 +0100

    no message

---
+++ Assets/Scenes/WelcomeRoom/Scripts/QuestManager/Quests/QI_Teleportation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+namespace WelcomeRoom.QuestManager
+{
+    public class QI_Teleportation : MonoBehaviour
+    {
+        private TeleportMarkerBase teleportingToMarker;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (teleportingToMarker.ShouldMovePlayer()) { Debug.Log("We should teleport@@@"); }
+        }
+    }
+}
