837a695cafc70b1538fd3b2a77017eca7656a87e
Saeed Safikhani <s.safikhani@outlook.com>
2020-01-26 18:29:23 +0100

    no message

--- Assets/Scenes/WelcomeRoom/Scripts/QuestManager/Quests/QI_MovePhys.cs
+++ Assets/Scenes/WelcomeRoom/Scripts/QuestManager/Quests/QI_MovePhys.cs
@@ -6,17 +6,22 @@ namespace WelcomeRoom.QuestManager
 {
     public class QI_MovePhys : MonoBehaviour, IQuest
     {
-        [SerializeField] float MovementThreshold = 1f;
+        [SerializeField] float MovementThreshold = 4f;
         Vector3 iniCameraPosition;
+        Vector3 PrevCamerapostion;
+        int runCycle;
+        public float SumDistance = 0f;
         void Start()
         {
-            iniCameraPosition = Camera.main.transform.position;
+            //iniCameraPosition = Camera.main.transform.position;
+            PrevCamerapostion = Camera.main.transform.position;
         }
 
         void Update()
         {
-            float diffPosition = (Camera.main.transform.position - iniCameraPosition).magnitude;
-            if (diffPosition > MovementThreshold)
+            SumDistance += (Camera.main.transform.position - PrevCamerapostion).magnitude;
+            PrevCamerapostion = Camera.main.transform.position;
+            if (SumDistance > MovementThreshold)
             {
                 Debug.Log("Ok, you walked enough!!!");
                 GetComponent<SubQuest>().isFinished = true;
