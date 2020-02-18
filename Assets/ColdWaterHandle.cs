aa3d9722c456b0a9a096f9336171add60656a8b3
saiedbaran <s.safikhani@outlook.com>
2020-02-18 12:32:37 +0100

    Add circular drive for refrigrator door and some other objects

---
+++ Assets/ColdWaterHandle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColdWaterHandle : MonoBehaviour
+{
+    public GameObject Water;
+
+
+    Material waterMaterial;
+    Vector3 waterIniScale;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
