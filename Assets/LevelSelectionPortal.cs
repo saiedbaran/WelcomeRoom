b165371f22cd42f09c3f5077866a33a10864189f
saiedbaran <s.safikhani@outlook.com>
2020-02-19 11:38:40 +0100

    OpenDoorFunction

---
+++ Assets/LevelSelectionPortal.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class LevelSelectionPortal : MonoBehaviour
+{
+    public GameObject DoorHandle;
+
+    public void ActiveDoorHandle()
+    {
+        DoorHandle.GetComponent<CircularDrive>().enabled = true;
+    }
+}
---
+++ Assets/Test_DoorHandle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_DoorHandle : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    public void ActiveDoor()
+    {
+        Vector3 iniPos = transform.position;
+        Vector3 iniRot = transform.rotation.eulerAngles;
+
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.isKinematic = false;
+
+        Debug.Log("Put Speed to Zero!!!");
+        rigidBody.velocity = Vector3.zero;
+        rigidBody.angularVelocity = Vector3.zero;
+
+        transform.position = iniPos;
+        transform.rotation = Quaternion.Euler(iniRot);
+    }
+}
